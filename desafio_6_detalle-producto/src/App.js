206e9af513247403246c0b84c702df1a7176214b
Nartroya <nlarott@gmail.com>
2022-08-07 19:55:32 -0500

    commit

--- desafio_6_detalle-producto/src/App.js
+++ desafio_6_detalle-producto/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import NavBar from './components/NavBar/';
 import ItemListContainer from './container/ItemListContainer';
 import ItemDetailContainer from './container/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar/>
+      <Routes>
+        <Route path='/' element={<ItemListContainer/>}></Route>
+        <Route path='/category/:id' element={<ItemListContainer/>}/>
+      </Routes>
       <ItemListContainer greeting={"Ejemplo títutlo para un ítem"}/>
       <ItemDetailContainer />
-    </>
+    </BrowserRouter>
 
     /*<>
       
